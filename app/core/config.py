de3a16e86ebbb0b26834031000da81a45d0e8242
mell191 <nikitakorobincev008@email.com>
2026-05-03 12:14:50 +0500

    Написан config.py

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
+    APP_NAME: str 
+    DEBUG: bool
+    DESCRIPTION: str
+    VERSION: str
+
+    RapidAPI_Key: str
+    RapidAPI_Host: str = "aerodatabox.p.rapidapi.com"
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+
+settings = Settings() 
