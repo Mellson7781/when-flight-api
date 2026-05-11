4723bd46c4967292c8519d086ea5f848cb88b751
mell191 <nikitakorobincev008@email.com>
2026-05-11 12:43:32 +0500

    Тест ci/cd

--- app/api/routers/ping.py
+++ app/api/routers/ping.py
@@ -9,3 +9,8 @@ ping_rt = APIRouter(prefix="/api/ping", tags=[Tags.SYSTEM])
 @ping_rt.get("/")
 async def ping():
     return {"message": "pong"}
+
+
+@ping_rt.get("/test")
+async def test():
+    return {"message": "Hello World!"}
